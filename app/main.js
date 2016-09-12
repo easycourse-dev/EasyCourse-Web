07deed039992dc863596b7418e55a77b397cc044
Mark Wen <markwenguojie94@gmail.com>
2016-09-12 00:53:41 -0400

    Moving on with redux

--- app/main.js
+++ app/main.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware, { Provider } from 'react-redux';
+import api from './redux/api.js';
+import reducer from './redux/reducers/reducer.js';
 import App from './App.jsx';
 import './css/index.css'; // Importing all the CSS files
+
+let createStoreWithMiddleware= applyMiddleware(thunkMiddleware, api)(createStore);
+
+let store = createStoreWithMiddleware(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
