8c6be2027c5b06b16c757e7d9791777c3812eea6
Mark Wen <markwenguojie94@gmail.com>
2016-10-26 21:03:28 -0400

    Cleaned up some code and added link to join us form

--- app/App.jsx
+++ app/App.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import {
-  Router,
-  Route
-} from 'react-router';
+import { Router, Route } from 'react-router';
 import { Public, Home } from 'jsx';
 import { NavBar, Footer } from 'components';
 import './css/index.css'; // Importing all the CSS files
-import store, { history } from './jsx/redux/Store.js'
+import store, { history } from './jsx/redux/Store.js';
 
 class App extends Component {
   render() {
