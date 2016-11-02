f35ba8e0c6e146cb990c23d98171a5f7e34e9c8e
Mark Wen <markwenguojie94@gmail.com>
2016-11-01 22:42:09 -0400

    Reformatted SignIn LogIn SignUp

--- app/jsx/components/login.jsx
+++ app/jsx/components/login.jsx
@@ -16,23 +16,17 @@ class Login extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <div className="LoginPageBackground">
-        <div className="container LoginPageWrapper">
-          <Row className="LoginPage">
-            <Col lg={4} lgOffset={4} md={6} mdOffset={3} sm={8} smOffset={2}>
-              <h2 className="PageTitle">
-                Log In
-              </h2>
-              <form onSubmit={handleSubmit(this.submit)}>
-                <FormGroup className="loginForm">
-                  <Field className="form-control" name="email" component="input" type="text" placeholder="Email"/>
-                  <Field className="form-control" name="password" component="input" type="password" placeholder="Password"/>
-                  <Button className="loginFormSubmitButton" bsStyle="primary" type="submit">Login</Button>
-                </FormGroup>
-              </form>
-            </Col>
-          </Row>
-        </div>
+      <div>
+        <h2 className="PageTitle">
+          Log In
+        </h2>
+        <form onSubmit={handleSubmit(this.submit)}>
+          <FormGroup className="loginForm">
+            <Field className="form-control" name="email" component="input" type="text" placeholder="Email"/>
+            <Field className="form-control" name="password" component="input" type="password" placeholder="Password"/>
+            <Button className="loginFormSubmitButton" bsStyle="primary" type="submit">Login</Button>
+          </FormGroup>
+        </form>
       </div>
     );
   }
