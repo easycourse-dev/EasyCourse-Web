c7dff6269b797c7afd91c251bf6f0978ce765326
Charlie Roth <roth47@purdue.edu>
2017-01-01 17:46:56 -0500

    Comment out pre app load authentication logic.

--- src/index.js
+++ src/index.js
@@ -27,28 +27,28 @@ function getHomeComponent() {
 }
 
 
-if (localStorage.getItem('authToken')) {
-  let authToken = localStorage.getItem('authToken')
-  let socket = io.connect('https://zengjintaotest.com/', { query: `token=${authToken}` })
-  socket.on('connect', () => {
-    socket.emit('syncUser', 1, (data, error) => {
-      console.log('Connected User: ', data.user);
-      if (data.user.joinedRoom.length === 0) {
-        store.dispatch({ type: USER_INITIAL_SIGNUP_SUCCESS })
-        browserHistory.push('/signin');
-      } else {
-        store.dispatch({
-          type: USER_AUTHENTICATE_SUCCESS,
-          payload: data.user
-        })
-        browserHistory.push('/');
-      }
-    })
-  })
-} else {
-  store.dispatch({ type: USER_AUTHENTICATE_FAILURE })
-  browserHistory.push('/');
-}
+// if (localStorage.getItem('authToken')) {
+//   let authToken = localStorage.getItem('authToken')
+//   let socket = io.connect('https://zengjintaotest.com/', { query: `token=${authToken}` })
+//   socket.on('connect', () => {
+//     socket.emit('syncUser', 1, (data, error) => {
+//       console.log('Connected User: ', data.user);
+//       if (data.user.joinedRoom.length === 0) {
+//         store.dispatch({ type: USER_INITIAL_SIGNUP_SUCCESS })
+//         browserHistory.push('/signin');
+//       } else {
+//         store.dispatch({
+//           type: USER_AUTHENTICATE_SUCCESS,
+//           payload: data.user
+//         })
+//         browserHistory.push('/');
+//       }
+//     })
+//   })
+// } else {
+//   store.dispatch({ type: USER_AUTHENTICATE_FAILURE })
+//   browserHistory.push('/');
+// }
 
 
 ReactDOM.render(
